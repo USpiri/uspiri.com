9a3a110d660b8b1da1194441595482d067b9b1b0
USpiri <urielspiridione135@gmail.com>
2026-06-10 00:52:12 -0300

    feat: add RSS feed support and integrate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.uspiri.com",
   base: "/",
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
