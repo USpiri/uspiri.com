973f6b744e3da0afe8fd389cd5b6005109e66b1b
USpiri <urielspiridione135@gmail.com>
2026-06-27 01:07:16 -0300

    feat: remove base path configuration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.uspiri.com",
-  base: "/",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
