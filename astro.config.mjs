229dfc3a7fbdf630afee36dd3353dc9c1f7a7cd9
USpiri <urielspiridione135@gmail.com>
2026-06-27 21:58:57 -0300

    fix: update site URL in configuration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.uspiri.com",
+  site: "https://uspiri.com",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
